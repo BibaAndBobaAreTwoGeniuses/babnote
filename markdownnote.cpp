732a44fd39d4dc5676225dfc4ef27427b0d6704d
klewy <cigankov807@gmail.com>
2025-01-07 13:22:10 +0300

    m

--- markdownnote.cpp
+++ markdownnote.cpp
@@ -8,17 +8,19 @@ MarkdownNote::MarkdownNote(QObject *parent)
 
 QString MarkdownNote::serialize()
 {
+    // тут ничо не надо сериализовывать можно и просто как текст хранить, а в канбане уже надо будет
     return m_contents;
 }
 
 void MarkdownNote::deserialize(const QString &json_contents)
 {
-    // Some insense deserialization process...
+    // Десериализовывать тоже ничо не надо
     m_contents = json_contents;
 }
 
 QString MarkdownNote::getType()
 {
+    // Мб сделать енам с типами?
     return "markdown";
 }
 
@@ -29,6 +31,7 @@ QString MarkdownNote::contents()
 
 void MarkdownNote::setContents(QString new_contents)
 {
+    // при автосохранении (или ручном сохранении) вызывается этот метод
     if (new_contents != m_contents) {
         m_contents = new_contents;
         emit contentsChanged();
